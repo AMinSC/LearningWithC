2cafb134c134e139f2b79551f98a4984ea70a7c3
AMinSC <cdtjdcjf@gmail.com>
2024-09-22 21:03:03 +0900

    macOS 기준 c 코드를 windowsOS 기준으로 리팩토링: `sys/time.h`와 같은 라이브러리는 운영체제 간의 차이로 인해 사용 가능 여부와 구현 방식이 다를 수 있음

--- testProject/gettimeofday.c
+++ testProject/gettimeofday.c
@@ -1,40 +1,51 @@
-#include <sys/time.h>
-#include <unistd.h>
+#include <Windows.h>
 #include <stdio.h>
 
 struct MyStructure {
-	int myNum;
-	char myLetter;
+    int myNum;
+    char myLetter;
 };
 
-int main(void) {
-	struct timeval startTime, endTime;
-	double diffTime, diffTime_2;
-	struct MyStructure s1;
-
-	gettimeofday(&startTime, NULL);
-	s1.myNum = 13;
-	s1.myLetter = 'B';
-	
-	printf("My number : %d\n", s1.myNum);
-	printf("My letter : %c\n", s1.myLetter);
-	sleep(1);
-	//usleep(700000);
-	gettimeofday(&endTime, NULL);
-	diffTime = ( endTime.tv_sec - startTime.tv_sec ) + (( endTime.tv_usec - startTime.tv_usec) / CLOCKS_PER_SEC); // 1000000 );
-	printf("%f s\n", diffTime);
-
-	gettimeofday(&startTime, NULL);
-	s1.myNum = 15;
-	s1.myLetter = 'A';
-
-	printf("My number : %d\n", s1.myNum);
-	printf("My letter : %c\n", s1.myLetter);
-	usleep(700000);
-	gettimeofday(&endTime, NULL);
-	diffTime_2 = (endTime.tv_usec - startTime.tv_usec);
-	printf("%f s\n", diffTime_2);
-	return 0;
+// QueryPerformanceCounter를 사용하여 시간 차이를 계산하는 함수
+double getElapsedTime(LARGE_INTEGER start, LARGE_INTEGER end, LARGE_INTEGER frequency) {
+    return (double)(end.QuadPart - start.QuadPart) / frequency.QuadPart;
 }
 
+int main(void) {
+    LARGE_INTEGER startTime, endTime, frequency;
+    double diffTime, diffTime_2;
+    struct MyStructure s1;
 
+    // 고해상도 타이머의 주파수를 구함
+    QueryPerformanceFrequency(&frequency);
+
+    // 첫 번째 시간 측정
+    QueryPerformanceCounter(&startTime);
+    s1.myNum = 13;
+    s1.myLetter = 'B';
+
+    printf("My number : %d\n", s1.myNum);
+    printf("My letter : %c\n", s1.myLetter);
+    Sleep(1000);  // 1초 대기 (1000 밀리초)
+
+    // 첫 번째 시간 측정 종료
+    QueryPerformanceCounter(&endTime);
+    diffTime = getElapsedTime(startTime, endTime, frequency);
+    printf("%f s\n", diffTime);
+
+    // 두 번째 시간 측정
+    QueryPerformanceCounter(&startTime);
+    s1.myNum = 15;
+    s1.myLetter = 'A';
+
+    printf("My number : %d\n", s1.myNum);
+    printf("My letter : %c\n", s1.myLetter);
+    Sleep(700);  // 700 밀리초 대기
+
+    // 두 번째 시간 측정 종료
+    QueryPerformanceCounter(&endTime);
+    diffTime_2 = getElapsedTime(startTime, endTime, frequency);
+    printf("%f s\n", diffTime_2);
+
+    return 0;
+}
