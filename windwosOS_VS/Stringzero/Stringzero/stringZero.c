39a604dce10a85f83d76f813325472208fa35e5c
AMinSC <cdtjdcjf@gmail.com>
2024-09-25 22:33:12 +0900

    fgets() 함수 추가

--- windwosOS_VS/Stringzero/Stringzero/stringZero.c
+++ windwosOS_VS/Stringzero/Stringzero/stringZero.c
@@ -5,10 +5,11 @@ int main(void)
 	char szName[32] = { 0 };
 
 	printf("이름을 입력하세요: ");
+	// gets()는 보안 결함 문제가 있음
+	// fgets(szName, sizeof(szName), stdin);  Unix 계열
 	gets_s(szName, sizeof(szName));
 
 	printf("당신의 이름은 ");
-	// gets()는 보안 결함 문제가 있음
 	puts(szName);
 	puts("입니다.");
 	return 0;
