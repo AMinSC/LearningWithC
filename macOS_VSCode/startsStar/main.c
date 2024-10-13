4899dd14a72a08e429244e8df6da266e92735f79
AMinSC <cdtjdcjf@gmail.com>
2024-10-13 11:30:19 +0900

    Big stars

---
+++ macOS_VSCode/startsStar/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int sizeNum = 0;
+    printf("별의 크기를 입력해주세요. ");
+    scanf("%d", &sizeNum);
+
+    
+
+    printf("큰별 \n");
+    for (int i = 0; i < sizeNum; i++)
+    {
+        for (int j = sizeNum; j > i; j--)
+            putchar('\t');
+        for (int j = 0; j < i + 1; j++)
+            printf("*\t");
+        for (int j = 1; j < i + 1; j++)
+            printf("*\t");
+        putchar('\n');
+    }
+    for (int i = 0; i < sizeNum - 1; i++)
+    {
+        for (int j = 0; j <= i + 1; j++)
+            putchar('\t');
+        for (int j = sizeNum - 1; j > i; j--)
+            printf("*\t");
+        for (int j = sizeNum - 2; j > i; j--)
+            printf("*\t");
+        putchar('\n');
+    }
+    return 0;
+}
